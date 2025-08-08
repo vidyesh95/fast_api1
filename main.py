9f8d39625c97ce5d6de1384f39850deeca868268
Vidyesh Churi <vidyesh95@gmail.com>
2025-08-08 17:13:46 +0530

    security trial

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from typing import Annotated
+
 from fastapi import FastAPI
+from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded",
+        email="vidyesh95@gmail.com",
+        full_name="Vidyesh Churi"
+    )
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise Exception("User not found")
+    return user
 
 
 @app.get("/")
@@ -11,3 +40,13 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
+
+
+@app.get("/users/me", response_model=User)
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
