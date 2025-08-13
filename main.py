a422038a32f122a016784168c03a31a2386c0a44
Vidyesh Churi <vidyesh95@gmail.com>
2025-08-13 12:40:03 +0530

    feature: simple api

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/read-root")
+def read_root():
+    """
+    This is a path operation function.
+    It handles GET requests to the root path "/read-root"
+    """
+    return {"message": "Hello, World!"}
